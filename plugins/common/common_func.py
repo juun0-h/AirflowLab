2fb5b610c8734f592a2d37ae5ae8ce60015433db
juun0.han <juun0.han@gmail.com>
2024-08-25 19:05:45 +0900

    Add: test dag for op_kwargs

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -6,3 +6,14 @@ def regist(name, sex, *args):
     print(f"성별: {sex}")
     print(f"기타 options: {args}")
     print('등록 완료')
+
+def regist2(name, sex, *args, **kwargs):
+    print(f"이름: {name}")
+    print(f"성별: {sex}")
+    print(f"기타 options: {args}")
+    email = kwargs.get('email') or None
+    phone = kwargs.get('phone') or None
+    if email:
+        print(f"email: {email}")
+    if phone:
+        print(f"phone: {phone}")
