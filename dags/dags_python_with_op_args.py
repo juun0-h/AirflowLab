bb44017ddc45abcda98bde1fad28b72e6d63540b
juun0.han <juun0.han@gmail.com>
2024-08-25 18:56:45 +0900

    Add: test dag for op_args

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import pendulum, datetime
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id='dags_python_with_op_args',
+    schedule='30 6 * * *',
+    start_date=pendulum.datetime(2024, 8, 24, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist,
+        op_args=['juun0_han', 'man', 'kr', 'Seoul']
+    )
+
+    regist_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,8 @@
 def get_sftp():
     print('sftp 작업 시작')
+
+def regist(name, sex, *args):
+    print(f"이름: {name}")
+    print(f"성별: {sex}")
+    print(f"기타 options: {args}")
+    print('등록 완료')
