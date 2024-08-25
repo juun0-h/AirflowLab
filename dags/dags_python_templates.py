92fcba4f443d61f3636caa82cfaba6501fe045d2
juun0.han <juun0.han@gmail.com>
2024-08-25 20:44:26 +0900

    Add: test dag for checking templates

---
+++ dags/dags_python_templates.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+
+
+with DAG(
+    dag_id="dags_python_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2024, 8, 20, tz="Asia/Seoul"), # tz="UTC": 국제표준시간
+    catchup=False,
+) as dag:
+    
+    def func1(start_date, end_date, **kwargs):
+        print(f"start_date: {start_date}")
+        print(f"end_date: {end_date}")
+    
+    py_t1 = PythonOperator(
+        task_id="py_t1",
+        python_callable=func1,
+        op_kwargs={'start_date': '{{ data_interval_start | ds }}', 'end_date': '{{ data_interval_end | ds }}'},
+    )
+
+    @task(task_id='py_t2')
+    def py_t2(**kwargs):
+        print(kwargs)
+        print('ds: ', kwargs['ds'])
+        print('ts: ', kwargs['ts'])
+        print('data_interval_start: ', kwargs['data_interval_start'])
+        print('data_interval_end: ', kwargs['data_interval_end'])
+        print('task_instances: ', str(kwargs['ti']))
