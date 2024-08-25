ba595a56eceae10aaa3535c2cd856cb636469e37
juun0.han <juun0.han@gmail.com>
2024-08-25 20:09:40 +0900

    Add: test dag for showing templates

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,20 @@
+import random
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.decorators import task
+
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2024, 8, 24, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
