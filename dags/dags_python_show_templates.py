66a66bf3c8b4d8c5f9519e3cb68384c92d58a76a
juun0.han <juun0.han@gmail.com>
2024-08-25 20:10:38 +0900

    Update: DAG catchup to True

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -9,7 +9,7 @@ with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2024, 8, 24, tz="Asia/Seoul"),
-    catchup=False
+    catchup=True
 ) as dag:
     
     @task(task_id='python_task')
