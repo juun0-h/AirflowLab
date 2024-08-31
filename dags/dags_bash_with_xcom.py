70dd0b9b3c6b5d70ac1b673ff7f290e91a46c62b
juun0.han <juun0.han@gmail.com>
2024-08-31 12:50:26 +0900

    Add: DAG to test xcom_push & xcom_pull using python decorator and BashOperator

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -21,7 +21,7 @@ with DAG(
         env={'PUSHED_VALUE': "{{ ti.xcom_pull(key='bash_pushed') }}",
              'RETURN_VALUE': "{{ ti.xcom_pull(task_ids='bash_push') }}"},
         bash_command= "echo $PUSHED_VALUE && echo $RETURN_VALUE",
-        do_xcom_push=False
+        do_xcom_push=False  # xcom_push 저장 미사용
     )
 
     bash_push >> bash_pull
